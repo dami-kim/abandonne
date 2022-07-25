2e4d084bdfe72bd782b2fde7e762c82638903ee6
dami-kim <62894564+dami-kim@users.noreply.github.com>
2022-07-25 22:15:16 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -272,38 +272,38 @@ var FadeTransition = Barba.BaseTransition.extend({
     $(window).scrollTop(0);
     var that = this;
     that.done();
- /*   this.newContainer.classList.add("slide-in");
+    this.newContainer.classList.add("slide-in");
 
     var that = this;
     
     this.newContainer.addEventListener('animationend', function() {
       that.newContainer.classList.remove('slide-in');
       that.done();
-    }); */
+    }); 
     /**
      * this.newContainer is the HTMLElement of the new Container
      * At this stage newContainer is on the DOM (inside our #barba-container and with visibility: hidden)
      * Please note, newContainer is available just after newContainerLoading is resolved!
      */
 
-    // var _this = this;
-    // var $el = $(this.newContainer);
+    var _this = this;
+    var $el = $(this.newContainer);
 
-    // $(this.oldContainer).hide();
+    $(this.oldContainer).hide();
 
-    // $el.css({
-    //   visibility : 'visible',
-    //   opacity : 0
-    // });
+    $el.css({
+      visibility : 'visible',
+      opacity : 0
+    });
 
-    // $el.animate({ opacity: 1 }, 400, function() {
-    //   /**
-    //    * Do not forget to call .done() as soon your transition is finished!
-    //    * .done() will automatically remove from the DOM the old Container
-    //    */
+    $el.animate({ opacity: 1 }, 400, function() {
+      /**
+       * Do not forget to call .done() as soon your transition is finished!
+       * .done() will automatically remove from the DOM the old Container
+       */
 
-    //   _this.done();
-    // });
+      _this.done();
+    });
   },
 
   valid: function() {
